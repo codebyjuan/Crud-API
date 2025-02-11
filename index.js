42d9ac50d6854583f038daba8d83b899aeb392a7
Juan <jbellevue83022@gmail.com>
2025-02-10 23:17:24 -0600

    Added getting a product by its id

--- index.js
+++ index.js
@@ -19,6 +19,28 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+// Get all products
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// get product by id
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 // Create Product Route
 app.post("/api/products", async (req, res) => {
   try {
