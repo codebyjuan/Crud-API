9b104c8753c4430182b7d06e13c2eedfa1f6a419
Juan <jbellevue83022@gmail.com>
2025-02-10 22:47:20 -0600

    Added code to create a product as well as connect to the mongodb database

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
+const Product = require("./models/product.model");
 
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect", err));
 
+// Define Routes
+
+// Homepage Route
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// Create Product Route
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Start Server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
-
-app.get("/", function (req, res) {
-  res.send("Hello World");
 });
